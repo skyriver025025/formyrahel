6d46b4d9e3620635d5829a428011bd869a7ffd73
skyriver025025 <skyriver025025@gmail.com>
2026-02-14 16:07:29 +0700

    Update indexjs.js

--- indexjs.js
+++ indexjs.js
@@ -1,6 +1,6 @@
 
 const title = document.querySelector('.title')
-const text = "Happy Valentine's Day Rahel Sayang".split('')
+const text = "Happy Valentine's Day    Rahel Sayang".split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
